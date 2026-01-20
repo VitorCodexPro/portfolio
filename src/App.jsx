8ba163a228af392e6a4e45577e69a1cbfed17bbb
VitorCodexPro <vitor@github.com>
2026-01-20 20:32:37 -0300

    feat: add open graph meta tags and og image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import Footer from './components/Footer/Footer'
-import Home from './pages/Home'
+// import Footer from './components/Footer/Footer'
+// import Home from './pages/Home'
 
 function App() {
   return (
-    <>
-      <main>
-        <Home />
-      </main>
-      <Footer />
-    </>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      width: '100vw',
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: 'clamp(3rem, 10vw, 8rem)',
+      fontWeight: 'bold',
+      fontFamily: 'sans-serif',
+      letterSpacing: '0.2em',
+      textAlign: 'center',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      zIndex: 9999
+    }}>
+      LOADING...
+    </div>
   )
 }
 
